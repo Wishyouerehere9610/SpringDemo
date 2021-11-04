44b271215c740b4267f36030cfd04e00f9079f9d
dongziming <zdong@zhejianglab.com>
2021-11-04 15:39:18 +0800

    test postgis

--- src/main/java/com/example/springdemo/utils/jtsGis.java
+++ src/main/java/com/example/springdemo/utils/jtsGis.java
@@ -1,11 +1,11 @@
 package com.example.springdemo.utils;
 
+import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKTReader;
 
-import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -13,14 +13,15 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-public class jstGis {
+public class jtsGis {
 
     public static void main(String[] args) throws ParseException {
         GeometryFactory geometryFactory = new GeometryFactory();
         WKTReader reader = new WKTReader(geometryFactory);
         List<String> data = new ArrayList<>();
-        List<String> result = new ArrayList<>();
-        String sql = "select st_astext(t.geom) from gis.cn_shi_data t where t.gid<5;";
+        List<Geometry> result = new ArrayList<>();
+        String sql = "select st_astext(t.geom) from gis.osm_buildings_a_free_1 t where t.gid<10000;";
+        long startTime = System.currentTimeMillis();   //获取开始时间
         try {
             Class.forName("org.postgresql.Driver");
             Connection conn = DriverManager
@@ -36,6 +37,7 @@ public class jstGis {
             }
             st.close();
             conn.close();
+            System.out.println("Close database successfully");
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
@@ -43,11 +45,22 @@ public class jstGis {
         }
         for (int i = 0; i < data.size(); i++) {
             MultiPolygon multipolygon = (MultiPolygon) reader.read(data.get(i));
-            double area = multipolygon.getArea();
-            BigDecimal bd1 = new BigDecimal(area);
-            result.add(bd1.toPlainString());
+            //计算面积
+//            double area = multipolygon.getArea();
+//            BigDecimal bd1 = new BigDecimal(area);
+            //计算周长
+//            double perimeter = multipolygon.getLength();
+//            BigDecimal bd1 = new BigDecimal(perimeter);
+            //计算缓冲区
+            Geometry buffer = multipolygon.buffer(10);
+            result.add(buffer);
+//            result.add(bd1.toPlainString());
         }
-        System.out.println(result);
+//        System.out.println(result);
+        long endTime = System.currentTimeMillis(); //获取结束时间
+        double time = endTime - startTime;
+        System.out.println("程序运行时间： " + time + "ms");
+
 
     }
 }
--- src/main/java/com/example/springdemo/utils/postGis.java
+++ src/main/java/com/example/springdemo/utils/postGis.java
@@ -2,6 +2,7 @@ package com.example.springdemo.utils;
 
 import com.vividsolutions.jts.io.ParseException;
 
+import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +10,10 @@ import java.util.List;
 public class postGis {
     public static void main(String[] args) throws ParseException {
         List<String> data = new ArrayList<>();
-        String sql = "select st_area(t.geom) from gis.cn_shi_data t where t.gid<5;";
+        long startTime = System.currentTimeMillis();   //获取开始时间
+//        String sql = "select st_area(t.geom) from gis.osm_buildings_a_free_1 t where t.gid<100000;";//返回的是geometry编码
+//        String sql = "select st_perimeter(t.geom) from gis.osm_buildings_a_free_1 t where t.gid<10000;";
+        String sql = "select st_buffer(t.geom,10) from gis.osm_buildings_a_free_1 t where t.gid<10000;";
         try {
             Class.forName("org.postgresql.Driver");
             Connection conn = DriverManager
@@ -22,15 +26,18 @@ public class postGis {
                 String val = rs.getString(1);
                 data.add(val);
             }
-            System.out.println(data);
+//            System.out.println(data);
             st.close();
             conn.close();
+            System.out.println("Close database successfully");
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
-
+        long endTime = System.currentTimeMillis(); //获取结束时间
+        double time = endTime - startTime;
+        System.out.println("程序运行时间： " + time + "ms");
 
     }
 
