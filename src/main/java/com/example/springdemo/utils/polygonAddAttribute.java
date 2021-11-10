33c615096dc2db589483f85589aa2563ca595408
dongziming <zdong@zhejianglab.com>
2021-11-10 17:02:08 +0800

    test postgis

--- src/main/java/com/example/springdemo/utils/polygonAddAttribute.java
+++ src/main/java/com/example/springdemo/utils/polygonAddAttribute.java
@@ -13,12 +13,12 @@ import static com.example.springdemo.utils.postGisUtil.*;
 public class polygonAddAttribute {
     public static void main(String[] args) throws ParseException {
         String[] tableObjects = new String[]{"gis.osm_buildings_a_free_1w",
-                "gis.osm_buildings_a_free_10w"};
-//                "gis.osm_buildings_a_free_100w"};
+                "gis.osm_buildings_a_free_10w",
+                "gis.osm_buildings_a_free_100w"};
         HashMap map = new HashMap<>();
         map.put("gis.osm_buildings_a_free_1w", "1w数据");
         map.put("gis.osm_buildings_a_free_10w", "10w数据");
-//        map.put("gis.osm_buildings_a_free_100w", "100w数据");
+        map.put("gis.osm_buildings_a_free_100w", "100w数据");
         System.out.println("--------------------------postGis测试面数据面积属性生成----------------------------------------------------");
         for (String tableName : tableObjects) {
             long startTime = System.currentTimeMillis();   //获取开始时间
